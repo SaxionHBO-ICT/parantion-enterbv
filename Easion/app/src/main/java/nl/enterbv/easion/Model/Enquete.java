15e0a7a36298e4f812470815e9d14e9ec5c442f0
shandrolis <joepvdstaai@live.nl>
2016-05-31 21:21:58 +0200

    updated model classes

--- Easion/app/src/main/java/nl/enterbv/easion/Model/Enquete.java
+++ Easion/app/src/main/java/nl/enterbv/easion/Model/Enquete.java
@@ -1,9 +1,56 @@
 package nl.enterbv.easion.Model;
 
+import java.util.Date;
+
 /**
  * Created by RubenW on 25-5-2016.
  */
 public class Enquete {
+    private int unique_ID;
+    private Date creationDate;
+    private String sender, label, message, link;
+    private int progress, studentnumber;
 
+    public Enquete(int unique_ID, Date creationDate, String sender, String label, String message, String link, int progress, int studentnumber) {
+        this.unique_ID = unique_ID;
+        this.creationDate = creationDate;
+        this.sender = sender;
+        this.label = label;
+        this.message = message;
+        this.link = link;
+        this.progress = progress;
+        this.studentnumber = studentnumber;
+    }
 
+    public int getUnique_ID() {
+        return unique_ID;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public int getProgress() {
+        return progress;
+    }
+
+    public int getStudentnumber() {
+        return studentnumber;
+    }
 }
--- Easion/app/src/main/java/nl/enterbv/easion/Model/User.java
+++ Easion/app/src/main/java/nl/enterbv/easion/Model/User.java
@@ -1,7 +1,59 @@
 package nl.enterbv.easion.Model;
 
+import java.util.Date;
+
 /**
  * Created by RubenW on 25-5-2016.
  */
 public class User {
+    private String username, firstname, prefix, lastname, email, outstreamProfile;
+    private Date lastLoginDate, registrationDate;
+
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public User(String username, String firstname, String prefix, String lastname, String email, String outstreamProfile, Date lastLoginDate, Date registrationDate) {
+        this.username = username;
+        this.firstname = firstname;
+        this.prefix = prefix;
+        this.lastname = lastname;
+        this.email = email;
+        this.outstreamProfile = outstreamProfile;
+
+        this.registrationDate = registrationDate;this.lastLoginDate = lastLoginDate;
+}
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getOutstreamProfile() {
+        return outstreamProfile;
+    }
+
+    public Date getLastLoginDate() {
+        return lastLoginDate;
+    }
+
+    public Date getRegistrationDate() {
+        return registrationDate;
+    }
 }
