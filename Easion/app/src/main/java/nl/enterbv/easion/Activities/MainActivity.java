3ac0d84af213b984c3c4440674ab00e1c15e7076
shandrolis <joepvdstaai@live.nl>
2016-05-31 15:24:40 +0200

    navigation drawer werkend

--- Easion/app/src/main/java/nl/enterbv/easion/Activities/MainActivity.java
+++ Easion/app/src/main/java/nl/enterbv/easion/Activities/MainActivity.java
@@ -1,15 +1,115 @@
 package nl.enterbv.easion.Activities;
 
+import android.app.FragmentManager;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import nl.enterbv.easion.Fragments.ContactFragment;
+import nl.enterbv.easion.Fragments.EnquetesFragment;
+import nl.enterbv.easion.Fragments.HomeFragment;
+import nl.enterbv.easion.Fragments.InfoFragment;
 import nl.enterbv.easion.R;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+
+        NavigationView navigationView = (NavigationView)findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        toggle.syncState();
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        View headerView = navigationView.getHeaderView(0);
+
+       LinearLayout header = (LinearLayout)headerView.findViewById(R.id.header);
+        header.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                drawerLayout.closeDrawer(GravityCompat.START);
+            }
+        });
+
     }
+
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        toggle.syncState();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        FragmentManager fragmentManager = getFragmentManager();
+
+        if (id == R.id.nav_home) {
+            fragmentManager.beginTransaction()
+                    .replace(R.id.content_frame
+                            , new HomeFragment())
+                    .commit();
+        } else if (id == R.id.nav_enquetes) {
+            fragmentManager.beginTransaction()
+                    .replace(R.id.content_frame
+                            , new EnquetesFragment())
+                    .commit();
+        } else if (id == R.id.nav_info) {
+            fragmentManager.beginTransaction()
+                    .replace(R.id.content_frame
+                            , new InfoFragment())
+                    .commit();
+        } else if (id == R.id.nav_contact) {
+            fragmentManager.beginTransaction()
+                    .replace(R.id.content_frame
+                            , new ContactFragment())
+                    .commit();
+        }
+
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
 }
