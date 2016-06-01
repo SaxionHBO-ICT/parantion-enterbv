8deb974fb6004754d8b2518c79e2c77af4936710
shandrolis <joepvdstaai@live.nl>
2016-06-01 14:19:29 +0200

    awesome clickable plaatje in Info fragment

--- Easion/app/src/main/java/nl/enterbv/easion/Fragments/InfoFragment.java
+++ Easion/app/src/main/java/nl/enterbv/easion/Fragments/InfoFragment.java
@@ -1,5 +1,7 @@
 package nl.enterbv.easion.Fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
@@ -7,6 +9,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import nl.enterbv.easion.R;
 
@@ -21,6 +24,18 @@ public class InfoFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.fragment_info, container, false);
+
+
+        ImageView iv_icon = (ImageView)myView.findViewById(R.id.frag_info_icon_iv);
+        iv_icon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.parantion.com/"));
+                startActivity(browserIntent);
+            }
+        });
+
+
         return myView;
     }
 }
