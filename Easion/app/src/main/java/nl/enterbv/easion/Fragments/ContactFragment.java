d3cb950a4bb3334fdee99997d8bac2f7801f8309
shandrolis <joepvdstaai@live.nl>
2016-06-20 22:01:30 +0200

    changed the way textviews looked in settings fragment

--- Easion/app/src/main/java/nl/enterbv/easion/Fragments/ContactFragment.java
+++ Easion/app/src/main/java/nl/enterbv/easion/Fragments/ContactFragment.java
@@ -1,6 +1,7 @@
 package nl.enterbv.easion.Fragments;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -9,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import nl.enterbv.easion.R;
 
@@ -56,6 +58,29 @@ public class ContactFragment extends Fragment {
                 Intent websiteIntent = new Intent(Intent.ACTION_VIEW);
                 websiteIntent.setData(Uri.parse(url));
                 startActivity(websiteIntent);
+            }
+        });
+
+        TextView emailLink  = (TextView)myView.findViewById(R.id.contact_tv_email);
+        emailLink.setTextColor(Color.BLUE);
+        emailLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+                emailIntent.setType("message/rfc822");
+                emailIntent.setAction(Intent.ACTION_SENDTO);
+                emailIntent.setData(Uri.parse("mailto:" + "info@parantion.com"));
+
+
+                emailIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL,
+                        new String[]{""});
+                emailIntent.putExtra(android.content.Intent.EXTRA_CC, "");
+                emailIntent.putExtra(android.content.Intent.EXTRA_BCC, "");
+                emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT,
+                        "");
+
+                startActivity(emailIntent);
             }
         });
 
